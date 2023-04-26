9b67ab49b9d5dc42b6d63eb7a31f9cf516d4d495
DavileGonzaPe <davidgonzalezperez81@gmail.com>
2023-04-26 07:19:15 +0200

    Initial commit

---
+++ src/main/java/com/springboot/ec/ExplorecaliMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorecaliMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecaliMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/ec/ExplorecaliMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorecaliMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
